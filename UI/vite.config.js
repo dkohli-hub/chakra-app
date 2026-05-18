4b4f21b0b7928dfa075c8a9031e129d86229d3ae
Mayur Dhoble <mayurdhoble5@gmail.com>
2026-05-18 08:00:48 +0530

    Fix vite preview allowedHosts for Render

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   preview: {
-    allowedHosts: 'all',
+    allowedHosts: ['chakra-app-ui.onrender.com'],
     host: '0.0.0.0',
   },
   server: mode === 'development' ? {
