d3bc1de3dbdca9c9bed2632e83ad656207070381
Mayur Dhoble <mayurdhoble5@gmail.com>
2026-05-29 22:54:26 +0530

    Replace logout 'Out' text with SVG icon

--- UI/src/components/dashboard/KarmaKshetra.jsx
+++ UI/src/components/dashboard/KarmaKshetra.jsx
@@ -206,7 +206,12 @@ export default function KarmaKshetra() {
           {done > 0 && (
             <button onClick={handleClearCompleted} style={{ ...hBtn, background: T.surface, border: `1px solid ${T.red}`, color: T.red, flexShrink: 0 }}>✕</button>
           )}
-          <button onClick={logout} style={{ ...hBtn, background: T.surface, border: `1px solid ${T.border}`, color: T.text2, flexShrink: 0 }}>Out</button>
+          <button onClick={logout} title="Logout" style={{ ...hBtn, background: T.surface, border: `1px solid ${T.border}`, color: T.text2, flexShrink: 0 }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M16 13v-2H7V8l-5 4 5 4v-3z"/>
+              <path d="M20 3h-9a2 2 0 0 0-2 2v4h2V5h9v14h-9v-4H9v4a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2z"/>
+            </svg>
+          </button>
         </div>
       </header>
 
